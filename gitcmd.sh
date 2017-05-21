61b62323241d1bd331aef5a5e2163b6c8c37e662
Morkva74 <vvsavin@mail.ru>
2017-05-21 16:33:56 +0300

    added shell scripts

--- gitcmd.sh
+++ gitcmd.sh
@@ -2,4 +2,4 @@ git config --global user.email "vvsavin@mail.ru"
 git config --global user.name "Morkva74"
 git add .
 git commit -m 'from terminal'
-git push git push https://github.com/Morkva74/stepic_web_project master
+git push https://github.com/Morkva74/stepic_web_project master
