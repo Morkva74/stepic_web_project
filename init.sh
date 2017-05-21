5ae2949a836d69e6f0cfb06827ae610245cdf4d0
Morkva74 <vvsavin@mail.ru>
2017-05-22 00:22:32 +0300

    nginx.conf

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/dj.conf /etc/gunicorn.d/dj.conf
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
