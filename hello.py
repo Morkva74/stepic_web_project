33c6341b6aa72de195144d2433860ed32b7ccd48
Morkva74 <vvsavin@mail.ru>
2017-05-21 13:22:55 +0000

    from terminal

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
-def wsgi_application(environ, start_response):
-    # бизнес-логика
+def app(environ, start_response):
     status = '200 OK'
     headers = [ ('Content-Type', 'text/plain') ] 
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')] 
     start_response( status, headers )
-    return body
+    return ['\r\n'.join(environ['QUERY_STRING'].split('&'))]
