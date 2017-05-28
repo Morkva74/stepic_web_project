c2cd285be44bd704a70d9e8ad1cf2587d82aa566
Morkva74 <vvsavin@mail.ru>
2017-05-28 19:06:50 +0000

    from terminal

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,13 +5,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-#Question - вопрос
-#title - заголовок вопроса
-#text - полный текст вопроса
-#added_at - дата добавления вопроса
-#rating - рейтинг вопроса (число)
-#author - автор вопроса
-#likes - список пользователей, поставивших "лайк"
 class QuestionManager(models.Manager):      
     def new(self):
         return Question.objects.order_by('-added_at')
@@ -24,10 +17,10 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-	rating = models.IntegerField(default=0)
-	author = models.ForeignKey(User, related_name="question_author")
-	likes = models.ManyToManyField(User, related_name="question_likes", blank=True)
-	objects = QuestionManager()
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(User, related_name="question_likes", blank=True)
+    objects = QuestionManager()
 	
     def get_absolute_url(self):
         return '/question/%d/' % self.pk
@@ -36,16 +29,11 @@ class Question(models.Model):
         db_table = 'qaQuestions'
         ordering = ['-added_at']
 
-#Answer - ответ
-#text - текст ответа
-#added_at - дата добавления ответа
-#question - вопрос, к которому относится ответ
-#author - автор ответа
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-	question = models.ForeignKey(Question)
-	author = models.ForeignKey(User)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
 	
     class Meta:
         db_table = 'qaAnswer'
